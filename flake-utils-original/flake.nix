928f04eae787f564cfb7e77ada8db7e19bf77fc7
Pavel Roskin <plroskin@gmail.com>
2024-03-11 09:48:24 -0700

    Using nixpkgs-fmt for this demo
    Alejandra format might be a distraction for some readers.

--- flake-utils-original/flake.nix
+++ flake-utils-original/flake.nix
@@ -4,24 +4,27 @@
     flake-utils.url = "github:numtide/flake-utils/1ef2e671c3b0c19053962c07dbda38332dcebf26";
   };
   outputs = inputs:
-    inputs.flake-utils.lib.eachSystem ["aarch64-linux" "x86_64-linux"]
-    (system: let
-      cross-config = modules: let
-        pkgs = import inputs.nixpkgs {inherit system;};
-      in
-        pkgs.pkgsCross.aarch64-multiplatform.nixos {imports = modules;};
-    in rec {
-      nixosModules.base = {pkgs, ...}: {
-        fileSystems."/" = {
-          device = "/dev/disk/by-label/nixos";
-          fsType = "ext4";
-        };
-        boot.loader.grub.enable = false;
-        system.stateVersion = "23.11";
-      };
-      nixosConfigurations = {devkit = cross-config [nixosModules.base];};
-      packages = {
-        default = nixosConfigurations.devkit.config.system.build.toplevel;
-      };
-    });
+    inputs.flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ]
+      (system:
+        let
+          cross-config = modules:
+            let
+              pkgs = import inputs.nixpkgs { inherit system; };
+            in
+            pkgs.pkgsCross.aarch64-multiplatform.nixos { imports = modules; };
+        in
+        rec {
+          nixosModules.base = { pkgs, ... }: {
+            fileSystems."/" = {
+              device = "/dev/disk/by-label/nixos";
+              fsType = "ext4";
+            };
+            boot.loader.grub.enable = false;
+            system.stateVersion = "23.11";
+          };
+          nixosConfigurations.devkit = cross-config [ nixosModules.base ];
+          packages = {
+            default = nixosConfigurations.devkit.config.system.build.toplevel;
+          };
+        });
 }
